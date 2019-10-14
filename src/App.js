c914408c7d75a9e752e537b1f03af91ed1ffe8eb
Jesse Cummins <me@jessecummins.com>
2019-10-14 19:06:30 -0400

    Uncomment Navigation component in App.js

--- src/App.js
+++ src/App.js
@@ -5,8 +5,8 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-    {/*
       <Navigation />
+    {/*
       <Logo />
       <ImageLinkForm />
       <FaceRecognition />
