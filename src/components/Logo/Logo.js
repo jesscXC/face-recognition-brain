2380ed9aab3a4c1e1a16a5b61c0b919e1c59332c
Jesse Cummins <me@jessecummins.com>
2019-10-18 22:40:52 -0400

    Add logo image to Logo component

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Tilt from 'react-tilt';
 import './Logo.css';
+import logo_image from './logo_globe-network.png';
 
 const Logo = () => {
   return (
@@ -10,7 +11,9 @@ const Logo = () => {
         options={{ max: 55 }}
         style={{ height: 150, width: 150 }}
       >
-        <div className="Tilt-inner"> 👽 </div>
+        <div className="Tilt-inner">
+          <img src={logo_image} alt="logo" height="150" width="150"/>
+        </div>
       </Tilt>
     </div>
   );
