9d9e0e3c61fced2ca05c88b7f9d16c57bca208c6
Jesse Cummins <me@jessecummins.com>
2019-10-21 00:30:49 -0400

    Style and add ImageLinkForm to page

--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -3,7 +3,15 @@ import React from 'react';
 const ImageLinkForm = () => {
   return (
     <div>
-    
+      <p className="f3">
+        {'This magic brain will detect faces in your pictures. Give it a try!'}
+      </p>
+      <div>
+        <input type="text" className="f4 pa2 w-70 center" />
+        <button className="w-30 grow f4 link ph3 pv2 dib white bg-light-purple">
+          Detect
+        </button>
+      </div>
     </div>
   );
 };
